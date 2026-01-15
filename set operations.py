869a426e6c758acd971e70f9c1fbc1a386e05490
arraghul2008-droid <ar.raghul.2008@gmail.com>
2026-01-15 16:22:34 +0530

    Add files via upload

---
+++ set operations.py
@@ -0,0 +1,19 @@
+
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+
+union_set = E | N
+print(f"Union of E and N is {union_set}")
+
+
+intersection_set = E & N
+print(f"Intersection of E and N is {intersection_set}")
+
+
+difference_set = E - N
+print(f"Difference of E and N is {difference_set}")
+
+
+symmetric_difference_set = E ^ N
+print(f"Symmetric difference of E and N is {symmetric_difference_set}")
